0111c1af2519a1b510827d12beddb5a343835ceb
Lewis Wheeler <Web-Slinger@outlook.com>
2021-06-18 11:27:26 +0000

    Added portfolio route.

--- run.py
+++ run.py
@@ -10,6 +10,12 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/portfolio")
+def portfolio():
+    images = os.listdir('static/portfolio/')
+    return render_template("portfolio.html", images=images)
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
